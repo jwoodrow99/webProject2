a7c6925dc66663477fd2e2a40056e65fdef7f9c0
Will Storey <will@willstorey.ca>
2020-03-20 20:44:17 +0000

    Payment form setup

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -2,8 +2,49 @@
 
 @section('content')
 
+    <h2>Choose Payment Option</h2>
 
-    <h2>Total Price: ${{$currentUser}}</h2>
+    <h3>Pay In-Store</h3>
+    <form method="POST" action="{{ action('OrderController@store') }}" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <label for="pickupName">Pickup Name</label>
+        <input name="pickupName" type="text" value="{{ $currentUser->customer->name }}"><br/>
+        <button type="submit">Order</button>
+    </form>
 
-    <a href="{{ action('OrderController@store') }}">[CHECKOUT]</a>
+    <h3>Prepay Online</h3>
+    <form method="POST" action="{{ action('OrderController@store') }}" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <h4>Billing Address</h4>
+        <label for="name">Name</label>
+        <input name="name" type="text" value="{{ $currentUser->customer->name }}"><br/>
+
+        <label for="address">Address</label>
+        <input name="address" type="text" value="{{ $currentUser->customer->address }}"><br/>
+
+        <label for="city">City</label>
+        <input name="city" type="text" value="{{ $currentUser->customer->city }}"><br/>
+
+        <label for="province">Province</label>
+        <input name="province" type="text" value="{{ $currentUser->customer->province }}"><br/>
+
+        <label for="postal">Postal Code</label>
+        <input name="postal" type="text" value="{{ $currentUser->customer->postal }}"><br/>
+
+        <label for="phoneNumber">Phone Number</label>
+        <input name="phoneNumber" type="tel" value="{{ $currentUser->customer->phone }}"><br/>
+
+        <h4>Pickup Date</h4>
+        <label for="pickupDate">Date</label>
+        <input name="pickupDate" type="date"><br/>
+
+        <h4>Payment Information</h4>
+        <div id="stripe-element"></div>
+
+
+
+        <button type="submit">Order</button>
+    </form>
+
+{{--    <a href="{{ action('OrderController@store') }}">[CHECKOUT]</a>--}}
 @endsection
