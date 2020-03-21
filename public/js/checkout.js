ade93b31c5878e13c8a4630226eabffb4c576c46
Will Storey <will@willstorey.ca>
2020-03-21 03:17:29 +0000

    Stripe implemented more proper - Need to work on passing better data with it

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -46,28 +46,37 @@ const form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    stripe.createToken(card).then(function(result) {
+    // stripe.createToken(card).then(function(result) {
+    //     if (result.error) {
+    //         // Inform the user if there was an error.
+    //         const errorElement = document.getElementById('card-errors');
+    //         errorElement.textContent = result.error.message;
+    //     } else {
+    //         // Send the token to your server.
+    //         stripeTokenHandler(result.token);
+    //     }
+    // });
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                name: 'Test Case'
+            }
+        }
+    }).then(function(result) {
         if (result.error) {
-            // Inform the user if there was an error.
-            const errorElement = document.getElementById('card-errors');
-            errorElement.textContent = result.error.message;
+            // Show error to your customer (e.g., insufficient funds)
+            console.log(result.error.message);
         } else {
-            // Send the token to your server.
-            stripeTokenHandler(result.token);
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                // Show a success message to your customer
+                // There's a risk of the customer closing the window before callback
+                // execution. Set up a webhook or plugin to listen for the
+                // payment_intent.succeeded event that handles any business critical
+                // post-payment actions.
+            }
         }
     });
 });
-
-// Submit the form with the token ID.
-function stripeTokenHandler(token) {
-    // Insert the token ID into the form so it gets submitted to the server
-    const form = document.getElementById('payment-form');
-    const hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'stripeToken');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
-
-    // Submit the form
-    form.submit();
-}
