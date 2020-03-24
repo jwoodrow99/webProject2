da3ff9b86eb8cf25cb8842d80e0e051b9555bef0
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-03-24 17:45:46 -0400

    setting up grid for prod list

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
+<link href="{{ asset('css/products/products.index.css') }}" rel="stylesheet">
 
 @section('content')
-
-    <ol>
+    <h1>Products</h1>
+    <ol class="grid-container">
         @foreach($products as $product)
-            <a href=" {{ action('ProductController@show', $product->id) }} ">
-                <li>
-                    @isset($product->image)
-                        <img src="{{ asset('storage/' . $product->image) }}"><br>
-                    @endisset
-                    Name: {{$product->name}}<br>
-                    Price: {{$product->price}}<br>
-                </li>
-            </a>
-            <br><br>
+            <div class="prod-list">
+                <a href=" {{ action('ProductController@show', $product->id) }} ">
+                    <li>
+                        @isset($product->image)
+                            <img src="{{ asset('storage/' . $product->image) }}"><br>
+                        @endisset
+                        Name: {{$product->name}}<br>
+                        Price: {{$product->price}}<br>
+                    </li>
+                </a>
+            </div>
         @endforeach
     </ol>
 @endsection
