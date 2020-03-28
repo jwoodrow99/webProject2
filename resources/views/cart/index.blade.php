7265ac358b06306e0f1e0b16d74920a8f9486af6
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-03-28 15:24:43 -0400

    +layout 2 cart, minor color change in product.show

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,43 +3,50 @@
 
 @section('content')
     <h1>Shopping Cart</h1>
+    <div class="prod-container">
+        <ul>
+            @foreach($cart as $item)
+                <li>
+    {{--                Product Id: {{$item->id}}<br>--}}
+    {{--                Product Code: {{$item->product->product_code}}<br>--}}
+                        <div class="item-container">
+                            <div class="prod-image">
+                                <img src="{{ asset('storage/' . $item->image) }}">
+                            </div>
+                            <div class="prod-info">
+                                <p class="para"><span class="prod-name"> {{$item->product->name}}</span></p>
+                                <p class="para"><span class="prod-price">&dollar;{{$item->product->price}}</span> per 10lb. box</p>
 
-    <ul>
-        @foreach($cart as $item)
-            <li>
-{{--                Product Id: {{$item->id}}<br>--}}
-{{--                Product Code: {{$item->product->product_code}}<br>--}}
-                <div class="prod-container">
-                    <div class="prod-image">
-                        <img src="{{ asset('storage/' . $item->image) }}">
-                    </div>
-                    <div class="prod-info">
-                        <p class="para"><span class="prod-name"> {{$item->product->name}}</span></p>
-                        <p class="para"><span class="prod-price">&dollar;{{$item->product->price}}</span> per 10lb. box</p>
+                                <p class="para">Number of Boxes {{$item->quantity}}
+                                    <button class="prod-edit">
+                                        <a href="{{ action('CartController@edit', $item->id) }}">Edit</a>
+                                    </button>
+                                </p>
+                                <p class="para">Number of pieces for <strong> each box </strong>{{$item->size}}
+                                    <button class="prod-edit">
+                                        <a href="{{ action('CartController@edit', $item->id) }}">Edit</a>
+                                    </button>
+                                </p>
 
-                        <p class="para">Number of Boxes {{$item->quantity}}<a href="{{ action('CartController@edit', $item->id) }}"><span class="prod-edit"> [EDIT] </span></a></p>
-                        <p class="para">Number of pieces for <strong> each box </strong>{{$item->size}}<a href="{{ action('CartController@edit', $item->id) }}"><span class="prod-edit"> [EDIT] </span></a></p>
+                {{--                Total Price: ${{$item->product->price * $item->quantity}}<br>--}}
 
-        {{--                Total Price: ${{$item->product->price * $item->quantity}}<br>--}}
+                                <form method="POST" action="{{ action('CartController@destroy', $item->id) }}">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="Delete">
+                                </form>
+                            </div>
+                        </div>
+                </li>
+            @endforeach
+        </ul>
+        <div class="prod-sum">
+            <h2>Order Summary</h2>
+            <p class="para">Total Price: ${{$totalPrice}}</p>
 
-                        <form method="POST" action="{{ action('CartController@destroy', $item->id) }}">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <input type="submit" value="Delete">
-                        </form>
-                    </div>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-
-    <hr>
-    <div class="prod-sum">
-        <h2>Order Summary</h2>
-        <p class="para">Total Price: ${{$totalPrice}}</p>
-
-        <button class="prod-checkout">
-            <a href="{{ action('OrderController@create') }}">Checkout</a>
-        </button>
+            <button class="prod-checkout">
+                <a href="{{ action('OrderController@create') }}">Checkout</a>
+            </button>
+        </div>
     </div>
 @endsection
