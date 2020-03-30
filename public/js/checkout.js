18a9963fbfef3cc4dbaeb26a488963628ccaf48e
Will Storey <will@willstorey.ca>
2020-03-30 05:06:50 +0000

    Paypal progress

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -3,13 +3,19 @@ paypal.Buttons({
     createOrder: function () {
         return fetch('/order/checkout', {
             method: 'post',
+            credentials: "same-origin",
             headers: {
-                'x-csrf-token': document.head.querySelector('meta[name="csrf-token"]'),
-                'content-type': 'application/json'
+                'content-type': 'application/json',
+                'accept': 'application/json',
+                'x-requested-with': 'XMLHttpRequest',
+                'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
             }
         }).then(function(res) {
+            console.log(document.head.querySelector('meta[name="csrf-token"]').content);
+            console.log(res);
             return res.json();
         }).then(function(data) {
+            console.log(data);
             return data.orderID;
         });
     }
