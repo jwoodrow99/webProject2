582c6f0e1084255e13f7db7606324800302d238b
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-03-31 17:18:02 -0400

    added layout woeking on content

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,47 +1,69 @@
 @extends('layouts.app')
-
+<link href="{{ asset('css/orders/orders.index.css') }}" rel="stylesheet">
 @section('content')
+<h1>Your Orders</h1>
+<div class="order-container">
+    <div class="update-info">
+    {{--    @foreach($customer->customers as $customer)--}}
+    {{--    @if(!Auth::user()->hasRole('manager'))--}}
+    {{--        <button><a href="{{ action('OrderController@index', $customer->id) }}">Your Orders</a></button>--}}{{-- user doesnt have manager role but has access to manager view--}}
+    {{--    @else--}}
+    {{--        <a href="{{ action('AdminController@orders') }}">[VIEW ORDERS]</a>--}}
+    {{--    @endif--}}
+    {{--    <br/>--}}
+    {{--    <button><a href="{{ action('CustomerController@edit', $customer->id) }}">Update Your Information</a></button>--}}
+    {{--        @endforeach--}}
+        <p>Your Order</p>
+        <p>Account Information</p>
+    </div>
+    <div class="order-content">
 
-    <ol>
-        @foreach($user_orders as $order)
-                @if($order->deleted_at != null)
-                    <li style="color: tomato">
-                @else
-                    <li>
-                @endif
-                Order Id: {{$order->id}}<br>
-                Price: {{$order->price}}<br>
-                Paid: {{$order->paid}}<br>
-                Order Placed: {{$order->created_at}}<br>
-                Pickup Date: {{$order->pickup_date}}<br>
-                Products:
-                <ul>
-                    @foreach($order->products as $product)
+        <h2>Orders</h2>
+        <ol>
+
+            @foreach($user_orders as $order)
+                    @if($order->deleted_at != null)
+                        <li style="color: tomato">
+                    @else
                         <li>
-                            Name: {{$product->name}}<br>
-                            Size: {{$product->pivot->size}}<br>
-                            Quantity: {{$product->pivot->quantity}}<br>
-                            Unit Price: {{$product->price}}<br>
-                            Total Price: {{$product->pivot->price}}<br>
-                        </li>
-                    @endforeach
-                </ul>
+                    @endif
+                            <span class="order-info order-id">Order #{{$order->id}}</span><br>
+                    Price: {{$order->price}}<br>
+                    Paid: {{$order->paid}}<br>
+                    Order Placed: {{$order->created_at}}<br>
+                        Pickup Date: {{$order->pickup_date}}<br>
+                    Products:
+                    <ul>
+                        @foreach($order->products as $product)
+                            <li class="order-item">
+                                {{$product->pivot->size}} Box of {{$product->name}}<br>
+                                {{$product->pivot->quantity}} Boxes of {{$product->name}}<br>
+                                Pickup Date: {{$order->pickup_date}}<br>
+                                Unit Price: {{$product->price}}<br>
+                                Total Price: {{$product->pivot->price}}<br>
+                            </li>
+                            <br/>
+                        @endforeach
+                    </ul>
 
-                <form method="POST" action="{{ action('OrderController@reorder', $order->id) }}">
-                    {{ csrf_field() }}
-                    <input type="submit" value="[RE-ORDER]">
-                </form>
-
-                @if($order->pickup_date >= now())
-                    <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">
-                        {{ method_field('DELETE') }}
+                    <form method="POST" action="{{ action('OrderController@reorder', $order->id) }}">
                         {{ csrf_field() }}
-                        <input type="submit" value="[DELETE]">
+                        <span class="btn-rep"><input type="submit" value="Re-Purchase"></span>
                     </form>
-                @endif
-            </li>
-            <br>
-        @endforeach
-    </ol>
 
+
+                    @if($order->pickup_date >= now())
+                        <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <input type="submit" value="[DELETE]">
+                        </form>
+                    @endif
+                </li>
+                <br>
+            @endforeach
+
+        </ol>
+    </div>
+</div>
 @endsection
