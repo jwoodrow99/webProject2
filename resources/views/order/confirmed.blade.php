8ecd68f38a722c8ac38dc7f5420c30ad051f58bb
Will Storey <will@willstorey.ca>
2020-03-31 02:29:33 +0000

    Added info to order confirmation

--- resources/views/order/confirmed.blade.php
+++ resources/views/order/confirmed.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <h2>Order Confirmation</h2>
-    <h4>Thank you for your purchase! Your order will be ready for pickup on {{ $order->pickup_date }}</h4>
+    <p>Thank you for your purchase! Your order will be ready for pickup on {{ $order->pickup_date }}</p>
 
+    <h5>Order Details</h5>
+    <p>Order # {{ $order->id }}</p>
+    @foreach($order->products as $product)
+        <p>
+            {{$product->pivot->quantity}} {{$product->name}} | {{$product->pivot->price}}
+        </p>
+    @endforeach
+    <p>Pickup Date: {{ $order->pickup_date }}</p>
+    <p>Total: {{ $order->price }}</p>
 @endsection
