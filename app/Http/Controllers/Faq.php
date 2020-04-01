ac6cafe420f6a1c5abbf1d315bc9bde63b5df633
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-04-01 16:50:36 -0400

    created FAQ view, controller, route, css

---
+++ app/Http/Controllers/Faq.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Faq extends Controller
+{
+    //
+    function index () {
+        return view('.faq/faq');
+    }
+}
---
+++ resources/views/faq/faq.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+<link href="{{ asset('css/faq/faq.css') }}" rel="stylesheet">
+@section('content')
+    <h1>Frequently Asked Questions</h1>
+    <div class="container">
+        <div>
+            <p>
+            </p>
+            </div>
+        <p>
+        </p>
+        <div>
+        </div>
+    </div>
+
+@endsection
