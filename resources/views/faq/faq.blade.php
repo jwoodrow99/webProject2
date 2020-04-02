22fb750dede9aa3c56c3705ade5421a4ddb71d71
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-04-01 20:40:26 -0400

    added accordion dropdown

--- resources/views/faq/faq.blade.php
+++ resources/views/faq/faq.blade.php
@@ -11,7 +11,19 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie maximus nulla, ac faucibus lectus semper eu. Aliquam magna tellus, dapibus sit amet elit quis, condimentum porta diam. Vivamus sit amet mi posuere, congue dolor in, vulputate mauris. Curabitur diam elit, scelerisque sit amet leo vulputate, pretium bibendum turpis. Donec rutrum nisl nec leo efficitur, id pharetra augue rutrum. Aliquam fringilla, ex gravida venenatis blandit, dui metus mollis purus, vitae cursus erat dui at ex. Quisque vitae condimentum eros, vehicula hendrerit sem. Nulla facilisi. In nisl neque, bibendum nec turpis nec, dignissim sodales lectus. Integer turpis neque, auctor nec mi vehicula, gravida aliquam est. Aenean id nisl viverra, mattis lectus non, ultricies lorem. Proin in lorem at dui rhoncus faucibus rhoncus eu orci. Donec sollicitudin ante ut orci tempor pretium.
             </p>
         </div>
+        <button class="accordion">Lorem ipsum dolor sit amet?</button>
+        <div class="panel">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie maximus nulla, ac faucibus lectus semper eu. Aliquam magna tellus, dapibus sit amet elit quis, condimentum porta diam. Vivamus sit amet mi posuere, congue dolor in, vulputate mauris. Curabitur diam elit, scelerisque sit amet leo vulputate, pretium bibendum turpis. Donec rutrum nisl nec leo efficitur, id pharetra augue rutrum. Aliquam fringilla, ex gravida venenatis blandit, dui metus mollis purus, vitae cursus erat dui at ex. Quisque vitae condimentum eros, vehicula hendrerit sem. Nulla facilisi. In nisl neque, bibendum nec turpis nec, dignissim sodales lectus. Integer turpis neque, auctor nec mi vehicula, gravida aliquam est. Aenean id nisl viverra, mattis lectus non, ultricies lorem. Proin in lorem at dui rhoncus faucibus rhoncus eu orci. Donec sollicitudin ante ut orci tempor pretium.
+            </p>
+        </div>
         <h3>Aenean vel convallis velit</h3>
+        <button class="accordion">Lorem ipsum dolor sit amet, consectetur adipiscing elit?</button>
+        <div class="panel">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie maximus nulla, ac faucibus lectus semper eu. Aliquam magna tellus, dapibus sit amet elit quis, condimentum porta diam. Vivamus sit amet mi posuere, congue dolor in, vulputate mauris. Curabitur diam elit, scelerisque sit amet leo vulputate, pretium bibendum turpis. Donec rutrum nisl nec leo efficitur, id pharetra augue rutrum. Aliquam fringilla, ex gravida venenatis blandit, dui metus mollis purus, vitae cursus erat dui at ex. Quisque vitae condimentum eros, vehicula hendrerit sem. Nulla facilisi. In nisl neque, bibendum nec turpis nec, dignissim sodales lectus. Integer turpis neque, auctor nec mi vehicula, gravida aliquam est. Aenean id nisl viverra, mattis lectus non, ultricies lorem. Proin in lorem at dui rhoncus faucibus rhoncus eu orci. Donec sollicitudin ante ut orci tempor pretium.
+            </p>
+        </div>
         <button class="accordion">Lorem ipsum dolor sit amet, consectetur adipiscing elit?</button>
         <div class="panel">
             <p>
@@ -25,9 +37,15 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie maximus nulla, ac faucibus lectus semper eu. Aliquam magna tellus, dapibus sit amet elit quis, condimentum porta diam. Vivamus sit amet mi posuere, congue dolor in, vulputate mauris. Curabitur diam elit, scelerisque sit amet leo vulputate, pretium bibendum turpis. Donec rutrum nisl nec leo efficitur, id pharetra augue rutrum. Aliquam fringilla, ex gravida venenatis blandit, dui metus mollis purus, vitae cursus erat dui at ex. Quisque vitae condimentum eros, vehicula hendrerit sem. Nulla facilisi. In nisl neque, bibendum nec turpis nec, dignissim sodales lectus. Integer turpis neque, auctor nec mi vehicula, gravida aliquam est. Aenean id nisl viverra, mattis lectus non, ultricies lorem. Proin in lorem at dui rhoncus faucibus rhoncus eu orci. Donec sollicitudin ante ut orci tempor pretium.
             </p>
         </div>
-        <p>
-            <a class="" href="{{url('contactus')}}">Contact Us</a> for any other inquiries.
-        </p>
+        <button class="accordion">Integer turpis neque, auctor nec mi vehicula, gravida aliquam est?</button>
+        <div class="panel">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie maximus nulla, ac faucibus lectus semper eu. Aliquam magna tellus, dapibus sit amet elit quis, condimentum porta diam. Vivamus sit amet mi posuere, congue dolor in, vulputate mauris. Curabitur diam elit, scelerisque sit amet leo vulputate, pretium bibendum turpis. Donec rutrum nisl nec leo efficitur, id pharetra augue rutrum. Aliquam fringilla, ex gravida venenatis blandit, dui metus mollis purus, vitae cursus erat dui at ex. Quisque vitae condimentum eros, vehicula hendrerit sem. Nulla facilisi. In nisl neque, bibendum nec turpis nec, dignissim sodales lectus. Integer turpis neque, auctor nec mi vehicula, gravida aliquam est. Aenean id nisl viverra, mattis lectus non, ultricies lorem. Proin in lorem at dui rhoncus faucibus rhoncus eu orci. Donec sollicitudin ante ut orci tempor pretium.
+            </p>
+        </div>
+        <span class="contact">
+            <a href="{{url('contactus')}}">Contact Us</a> for any other inquiries.
+        </span>
     </div>
     <script>
     let acc = document.getElementsByClassName("accordion");
