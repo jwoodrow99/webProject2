bf84922b9ad40aa7336cb47b289da1321158a3cd
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-04-03 13:44:37 -0400

    current orders styles, needs orders to test!

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,14 +2,14 @@
 <link href="{{ asset('css/admin/admin.index.css') }}" rel="stylesheet">
 @section('content2')
     <section class="products">
-        <h1>Inventory</h1>
+        <h2>Inventory</h2>
         <div class="prod-heading">
             <span class="heading">Name</span>
             <span class="heading">Product Code</span>
             <span class="heading">Quantity</span>
             <span class="heading">Update stock</span>
         </div>
-{{--        <div class="prod-list">--}}
+        <div class="prod-list">
             @foreach($products as $product)
                 <div class="prod">
                     <span class="prod-item"> {{$product->name}}</span><br/>
@@ -38,46 +38,45 @@
                     </div>
                 </div>
             @endforeach
-{{--        </div>--}}
+        </div>
     </section>
     <a href="{{ action('ProductController@create') }}"><button class="add-prod-btn">Add New Product</button></a>
 
-{{--    <section class="todayOrder">--}}
-{{--        <h3>Todays Orders</h3>--}}
-{{--        <p>These are the orders that are due to be picked up today!</p>--}}
-{{--        <table>--}}
-{{--            <tr>--}}
-{{--                <th>Order Id</th>--}}
-{{--                <th>Customer</th>--}}
-{{--                <th>Pickup Date</th>--}}
-{{--                <th>Total</th>--}}
-{{--            </tr>--}}
-{{--            @foreach($orders as $order)--}}
-{{--                <tr>--}}
-{{--                    <td>{{$order->id}}</td>--}}
-{{--                    <td>{{$order->user->name}}</td>--}}
-{{--                    <td>{{$order->pickup_date}}</td>--}}
-{{--                    <td>{{$order->price}}</td>--}}
-{{--                    <td>--}}
-{{--                        <form method="post" action="{{ action('AdminController@pickedUp', $order->id) }}" enctype="multipart/form-data">--}}
-{{--                            {{ csrf_field() }}--}}
-{{--                            {{ method_field('PATCH') }}--}}
-{{--                            <input type="submit" value="Picked Up">--}}
-{{--                        </form>--}}
-{{--                    </td>--}}
-{{--                    <td>--}}
-{{--                        <a href="{{ action('OrderController@show', $order->id) }}">VIEW ORDER</a>--}}
-{{--                    </td>--}}
-{{--                    <td>--}}
-{{--                        <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">--}}
-{{--                            {{ method_field('DELETE') }}--}}
-{{--                            {{ csrf_field() }}--}}
-{{--                            <input type="submit" value="DELETE">--}}
-{{--                        </form>--}}
-{{--                    </td>--}}
-{{--                </tr>--}}
-{{--            @endforeach--}}
-{{--        </table>--}}
-{{--    </section>--}}
+    <section class="todayOrder">
+        <h2>Current Orders</h2>
+        <p>These are the orders that are due to be picked up today!</p>
+        <div class="order-heading">
+                <span class="heading"> Order Id</span>
+                <span class="heading"> Customer</span>
+                <span class="heading"> Pickup Date</span>
+                <span class="heading"> Total</span>
+        </div>
+        <div class="order-list">
+            @foreach($orders as $order)
+                <div class="order">
+                    <span class="order-item"> {{$order->id}} </span>
+                    <span class="order-item"> {{$order->user->name}}</span>
+                    <span class="order-item"> {{$order->pickup_date}}</span>
+                    <span class="order-item"> {{$order->price}}</span>
+
+                    <div class="order-btn">
+                        <form method="post" action="{{ action('AdminController@pickedUp', $order->id) }}" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            {{ method_field('PATCH') }}
+                            <input type="submit" value="Picked Up">
+                        </form>
+
+                        <a href="{{ action('OrderController@show', $order->id) }}"><button class="view-order-btn">VIEW ORDER</button></a>
+
+                        <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <input class="del-btn" type="submit" value="DELETE">
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 
 @endsection
