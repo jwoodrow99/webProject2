8bc903930e2c42b888c66755a8a225b7468814b2
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-04-04 12:44:20 -0400

    html & css layout for admin.order

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,72 +1,71 @@
 @extends('layouts.admin')
-
+<link href="{{ asset('css/admin/admin.orders.css') }}" rel="stylesheet">
 @section('content2')
-
-    <h3>All Orders</h3>
-
-    <table>
-        <tr style="border-bottom: 1px solid black">
-            <th>Order Id</th>
-            <th>Customer</th>
-            <th>Pickup Date</th>
-            <th>Total</th>
-            <th>Paid</th>
-            <th>Picked Up</th>
-        </tr>
+<div class="order-container">
+    <h2>All Orders</h2>
+    <div class="order-heading">
+            <span class="heading"> Order Id</span>
+            <span class="heading"> Customer</span>
+            <span class="heading"> Pickup Date</span>
+            <span class="heading"> Total</span>
+            <span class="heading"> Paid</span>
+            <span class="heading"> Picked Up</span>
+    </div>
+    <div class="order-list">
         @foreach($orders as $order)
-            <tr>
-                <td>{{$order->id}}</td>
-                <td>{{$order->user->name}}</td>
-                <td>{{$order->pickup_date}}</td>
-                <td>{{$order->price}}</td>
+            <div class="order">
+                <span class="order-item"> {{$order->id}}</span>
+                <span class="order-item"> {{$order->user->name}}</span>
+                <span class="order-item"> {{$order->pickup_date}}</span>
+                <span class="order-item"> {{$order->price}}</span>
 
                 @if($order->paid)
-                    <td style="color: green">PAID</td>
+                   <span class="order-item order-paid">PAID</span>
                 @else
-                    <td style="color: tomato">NOT PAID</td>
+                    <span class="order-item order-not-paid"> NOT PAID</span>
                 @endif
 
                 @if($order->picked_up)
-                    <td style="color: green">Picked Up</td>
+                    <span class="order-item order-picked-up"> Picked Up</span>
                 @else
-                    <td style="color: tomato">Waiting</td>
+                    <span class="order-item order-waiting"> Waiting</span>
                 @endif
 
-                <td>
-                    <a href="{{ action('OrderController@show', $order->id) }}">VIEW ORDER</a>
-                </td>
-                <td>
-                    @if($order->pickup_date >= now()->toDateString())
-                        <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <input type="submit" value="[DELETE]">
-                        </form>
-                    @endif
-                </td>
-            </tr>
-        @endforeach
-    </table>
 
-    <h2>Removed Orders</h2>
-    <table>
-        <tr>
-            <th>Order Id</th>
-            <th>Customer</th>
-            <th>Pickup Date</th>
-            <th>Total</th>
-            <th>View Button</th>
-        </tr>
-        @foreach($trashedOrders as $order)
-            <tr>
-                <td>{{$order->id}}</td>
-                <td>{{$order->user->name}}</td>
-                <td>{{$order->pickup_date}}</td>
-                <td>{{$order->price}}</td>
-                <td>
-                    <a href="{{ action('OrderController@show', $order->id) }}">VIEW ORDER</a>
-                </td>
-            </tr>
+                <span class="order-item"> <a href="{{ action('OrderController@show', $order->id) }}"><button>VIEW ORDER</button></a></span>
+
+
+                @if($order->pickup_date >= now()->toDateString())
+                    <form method="POST" action="{{ action('OrderController@destroy', $order->id) }}">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <input type="submit" value="[DELETE]">
+                    </form>
+                @endif
+            </div>
         @endforeach
-    </table>
+    </div>
+</div>
+
+{{--    <h2>Removed Orders</h2>--}}
+{{--    <table>--}}
+{{--        <tr>--}}
+{{--            <th>Order Id</th>--}}
+{{--            <th>Customer</th>--}}
+{{--            <th>Pickup Date</th>--}}
+{{--            <th>Total</th>--}}
+{{--            <th>View Button</th>--}}
+{{--        </tr>--}}
+{{--        @foreach($trashedOrders as $order)--}}
+{{--            <tr>--}}
+{{--                <td>{{$order->id}}</td>--}}
+{{--                <td>{{$order->user->name}}</td>--}}
+{{--                <td>{{$order->pickup_date}}</td>--}}
+{{--                <td>{{$order->price}}</td>--}}
+{{--                <td>--}}
+{{--                    <a href="{{ action('OrderController@show', $order->id) }}">VIEW ORDER</a>--}}
+{{--                </td>--}}
+{{--            </tr>--}}
+{{--        @endforeach--}}
+{{--    </table>--}}
 @endsection
