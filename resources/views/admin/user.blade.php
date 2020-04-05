c243e0b6026be7d987d3a22a74982004c5aa810d
Jd King <46791774+pseudokiing@users.noreply.github.com>
2020-04-05 16:16:42 -0400

    admin.user complete

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1,67 +1,75 @@
 @extends('layouts.admin')
-
+<link href="{{ asset('css/admin/admin.user.css') }}" rel="stylesheet">
 @section('content2')
-    <table>
-
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Details</th>
-            <th>Manager</th>
-            <th>Employee</th>
-        </tr>
-
+    <div class="user-container">
+        <h2>All Users</h2>
+        <div class="heading-container">
+            <div class="user-heading">
+{{--            <span class="heading"> Id</span>--}}
+                <span class="heading"> Name</span>
+                <span class="heading"> Email</span>
+                <span class="heading"> Phone</span>
+                <span class="heading"> Details</span>
+                <span class="heading"> Manager</span>
+                <span class="heading"> Employee</span>
+            </div>
+        </div>
+        <div class="user-list">
         @foreach($customers as $customer)
-            <tr>
-                <td>{{$customer->user->id}}</td>
-                <td>{{$customer->name}}</td>
-                <td>{{$customer->user->email}}</td>
-                <td>{{$customer->phone}}</td>
-                <td>
-                    <a href="{{ action('CustomerController@show', $customer->id) }}">VIEW</a>
-                </td>
+            <div class="user">
+{{--                <span class="user-item"> {{$customer->user->id}}</span>--}}
+                <span class="user-item"> {{$customer->name}}</span>
+                <span class="user-item"> {{$customer->user->email}}</span>
+                <span class="user-item"> {{$customer->phone}}</span>
 
-                <td>
+                    <span class="user-item"> <a href="{{ action('CustomerController@show', $customer->id) }}"><button class="view-user-btn"> View User</button></a></span>
+
+
+
                     @if($customer->user->hasRole('manager'))
-                        <form method="POST" action="{{ action('AdminController@removeRole', $customer->user->id) }}">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="role" value="manager">
-                            <input type="submit" value="REMOVE">
-                        </form>
+                        <span class="user-item">
+                            <form method="POST" action="{{ action('AdminController@removeRole', $customer->user->id) }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="role" value="manager">
+                                <input type="submit" value="Remove Manager" class="rmv-role-btn">
+                            </form>
+                        </span>
                     @else
-                        <form method="POST" action="{{ action('AdminController@addRole', $customer->user->id) }}">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="role" value="manager">
-                            <input type="submit" value="ADD">
-                        </form>
+                        <span class="user-item">
+                            <form method="POST" action="{{ action('AdminController@addRole', $customer->user->id) }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="role" value="manager">
+                                <input type="submit" value="Add Manager" class="add-role-btn">
+                            </form>
+                        </span>
                     @endif
-                </td>
 
-                <td>
                     @if($customer->user->hasRole('employee'))
-                        <form method="POST" action="{{ action('AdminController@removeRole', $customer->user->id) }}">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="role" value="employee">
-                            <input type="submit" value="REMOVE">
-                        </form>
+                        <span class="user-item">
+                            <form method="POST" action="{{ action('AdminController@removeRole', $customer->user->id) }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="role" value="employee">
+                                <input type="submit" value="Remove Employee" class="rmv-role-btn">
+                            </form>
+                        </span>
                     @else
-                        <form method="POST" action="{{ action('AdminController@addRole', $customer->user->id) }}">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="role" value="employee">
-                            <input type="submit" value="ADD">
-                        </form>
+                        <span class="user-item">
+                            <form method="POST" action="{{ action('AdminController@addRole', $customer->user->id) }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="role" value="employee">
+                                <input type="submit" value="Add Employee" class="add-role-btn">
+                            </form>
+                        </span>
                     @endif
-                </td>
-                <td>
-                    <form method="POST" action="{{ action('CustomerController@destroy', $customer->id) }}">
-                        {{ method_field('DELETE') }}
-                        {{ csrf_field() }}
-                        <input type="submit" value="[DELETE]">
-                    </form>
-                </td>
-            </tr>
+                    <span class="user-item">
+                        <form method="POST" action="{{ action('CustomerController@destroy', $customer->id) }}">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <input type="submit" value="Delete User" class="del-usr-btn">
+                        </form>
+                    </span>
+            </div>
         @endforeach
-    </table>
+        </div>
+    </div>
 @endsection
