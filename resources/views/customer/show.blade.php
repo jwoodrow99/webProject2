709a2def7ecec5112682440cae8df8dfaf444810
jdkingin <jaideeking5@gmail.com>
2020-04-09 16:18:09 -0400

    customer.show finished

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -1,30 +1,45 @@
 
 @extends('layouts.app')
-
+<link href="{{ asset('css/customer/customer.show.css') }}" rel="stylesheet">
 @section('content')
+    <h1>User Information</h1>
+    <div class="usr-info-container">
+        <div class="usr-info">
+            <span class="field-title">
+                User Id:<br/>
+                Name:<br/>
+                Address:<br/>
+                City:<br/>
+                Province:<br/>
+                Postal:<br/>
+                Phone:<br/>
+                Newsletter:<br/>
+            </span>
+            <span class="usr-data">
+                {{ $customer->user_id  }}<br/>
+                {{ $customer->name  }}<br/>
+                {{ $customer->address  }}<br/>
+                {{ $customer->city  }}<br/>
+                {{ $customer->province  }}<br/>
+                {{ $customer->postal  }}<br/>
+                {{ $customer->phone  }}<br/>
+                {{ $customer->user->newsletter ? 'Subscribed' : 'Not Subscribed' }}<br/>
+            </span>
+        </div>
 
-    User Id: {{ $customer->user_id  }} <br>
-    Name: {{ $customer->name  }} <br>
-    Address: {{ $customer->address  }} <br>
-    City: {{ $customer->city  }} <br>
-    Province: {{ $customer->province  }} <br>
-    Postal: {{ $customer->postal  }} <br>
-    Phone: {{ $customer->phone  }} <br>
-    Subscribed to the newsletter: {{ $customer->user->newsletter ? 'TRUE' : 'FALSE' }}<br><br>
+        <a href="{{ action('CustomerController@edit', $customer->id) }}"><button>Edit</button></a>
 
-    <a href="{{ action('CustomerController@edit', $customer->id) }}">[EDIT]</a>
+        @if(!Auth::user()->hasRole('manager'))
+            <a href="{{ action('OrderController@index', $customer->id) }}"><button>View Orders</button></a>
+        @else
+            <a href="{{ action('AdminController@orders') }}"><button>View Orders</button></a>
+        @endif
 
-    @if(!Auth::user()->hasRole('manager'))
-        <a href="{{ action('OrderController@index', $customer->id) }}">[VIEW ORDERS]</a>
-    @else
-        <a href="{{ action('AdminController@orders') }}">[VIEW ORDERS]</a>
-    @endif
-
-    <p>You can only delete your account if you do not have any open orders!</p>
-    <form method="POST" action="{{ action('CustomerController@destroy', $customer->id) }}">
-        {{ method_field('DELETE') }}
-        {{ csrf_field() }}
-        <input type="submit" value="[DELETE]">
-    </form>
-
+        <p>You can only delete your account if you do not have any open orders!</p>
+        <form method="POST" action="{{ action('CustomerController@destroy', $customer->id) }}">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <input type="submit" value="Delete User">
+        </form>
+    </div>
 @endsection
