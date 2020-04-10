bde64e909af8d08e8eed1a88438c0e488be28687
jdkingin <jaideeking5@gmail.com>
2020-04-10 01:03:18 -0400

    fixes

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
-
+<link href="{{ asset('css/products/product.create.css') }}" rel="stylesheet">
 @section('content')
+    <h1>Add new product</h1>
+    <div class="add-prod-container">
 
-    <form method="post" action="{{ action('ProductController@store') }}" enctype="multipart/form-data">
+        <form method="post" action="{{ action('ProductController@store') }}" enctype="multipart/form-data">
 
-        {{ csrf_field() }}
-
-        <label for="product_code">Code: </label>
-        <input name="product_code" type="text" required><br>
-
-        <label for="name">Name: </label>
-        <input name="name" type="text" required><br>
-
-        <label for="description">description: </label>
-        <input name="description" type="text" required><br>
-
-        <label for="quantity">Quantity: </label>
-        <input name="quantity" type="text" required><br>
-
-        <label for="price">Price: </label>
-        <input name="price" type="text" required><br>
-
-        <label for="image">Image: </label>
-        <input name="image" type="file" accept="image/*" required><br>
-
-        <input type="submit" value="Create!">
-    </form>
-
+            {{ csrf_field() }}
+            <div class="add-prod-fields">
+                <span>
+                    <label for="product_code">Product Code: </label><br/>
+                    <label for="name">Product Name: </label><br/>
+                    <label for="description">Product Description: </label><br/>
+                    <label for="quantity">Product Quantity: </label><br/>
+                    <label for="price">Product Price: </label><br/>
+                    <label for="image">Product Image: </label><br/>
+                </span>
+                <span>
+                    <input name="product_code" type="text" required><br>
+                    <input name="name" type="text" required><br>
+                    <input name="description" type="text" required><br>
+                    <input name="quantity" type="text" required><br>
+                    <input name="price" type="text" required><br>
+                    <input name="image" type="file" accept="image/*" required><br>
+                    <input type="submit" value="Create!">
+                </span>
+            </div>
+        </form>
+    </div>
 @endsection
